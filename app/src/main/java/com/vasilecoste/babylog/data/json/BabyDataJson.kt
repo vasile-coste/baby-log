ea3facd34d3bd280f77aa038bb7c70cfa00cf4cf
Vasile Coste <vali.cv89@gmail.com>
2026-07-31 20:49:02 +0300

    fix bug on import

--- app/src/main/java/com/vasilecoste/babylog/data/json/BabyDataJson.kt
+++ app/src/main/java/com/vasilecoste/babylog/data/json/BabyDataJson.kt
@@ -66,7 +66,7 @@ object BabyDataJson {
             ImportedSleep(
                 date = LocalDate.parse(obj.getString("date")),
                 startTime = LocalTime.parse(obj.getString("startTime")),
-                endTime = LocalTime.parse(obj.getString("endTime")),
+                endTime = obj.optString("endTime").takeIf { it.isNotBlank() && it != "null" }?.let { LocalTime.parse(it) },
             )
         }
 
@@ -152,7 +152,7 @@ object BabyDataJson {
                     JSONObject()
                         .put("date", s.date.toString())
                         .put("startTime", s.startTime.toString())
-                        .put("endTime", s.endTime.toString())
+                        .put("endTime", s.endTime?.toString())
                 },
             ),
         )
